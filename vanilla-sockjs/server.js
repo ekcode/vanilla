c3588bed44d0602ac802563ef0a4e92327761cf8
Ickhyun Kwon <ekcode@icloud.com>
2017-03-03 17:59:02 +0900

    initial commit, basic chat room

---
+++ vanilla-sockjs/server.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var sockjs = require('sockjs');
+
+var echo = sockjs.createServer({ sockjs_url: 'http://cdn.jsdelivr.net/sockjs/1.0.1/sockjs.min.js' });
+
+
+var family = []
+echo.on('connection', function(conn) {
+    family.push(conn);
+    console.log(family);
+        
+    conn.on('data', function(message) {
+        family.forEach(function (c, key) {
+            c.write(message);
+        });
+    });
+    conn.on('close', function() {
+        console.log('close me ', conn.id);
+        family.splice(family.indexOf(conn), 1);  
+    });
+});
+
+var server = http.createServer();
+echo.installHandlers(server, {prefix:'/echo'});
+server.listen(9999, '0.0.0.0');
+
