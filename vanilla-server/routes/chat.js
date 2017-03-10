acde847d79848fb3155ffd029b39b5ac310ef48a
Ickhyun Kwon <ekcode@icloud.com>
2017-03-10 16:41:39 +0900

    detect my message and others

--- vanilla-server/routes/chat.js
+++ vanilla-server/routes/chat.js
@@ -4,7 +4,7 @@ var moment = require('moment');
 
 router.get('/:chatId', function(req, res, next) {
     console.log(req.params.chatId);
-    res.render('chat', { chatId: req.params.chatId, closeAt: '2017-03-10T15:55:12+09:00' });
+    res.render('chat', { chatId: req.params.chatId, closeAt: '2017-03-10T18:55:12+09:00' });
 });
 
 module.exports = router;
--- vanilla-sockjs/server.js
+++ vanilla-sockjs/server.js
@@ -6,14 +6,21 @@ var echo = sockjs.createServer({ sockjs_url: 'http://cdn.jsdelivr.net/sockjs/1.0
 
 var chatMembers = []
 echo.on('connection', function(conn) {
+    console.log(conn.id);
     chatMembers.push(conn);
-    console.log(chatMembers.chatId);
+    var auth = {};
+    auth.type = 'AUTH';
+    auth.id = conn.id;
+    conn.write(JSON.stringify(auth));
         
     conn.on('data', function(messageBody) {
         chatMembers.forEach(function (c, key) {
-            console.log(messageBody);
-            messageBody.id = c.id;
-            c.write(messageBody);
+            var res = {};
+            res.message = JSON.parse(messageBody).message;
+            res.loginId = JSON.parse(messageBody).loginId;
+            res.id = conn.id;
+            console.log(JSON.stringify(res));
+            c.write(JSON.stringify(res));
         });
     });
     conn.on('close', function() {
