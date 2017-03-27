4c4d70b6dcf9bad128cb934c0e5daa3cf4efdd16
Ickhyun Kwon <ekcode@icloud.com>
2017-03-27 10:25:46 +0900

    refactor sockjs server.js

--- vanilla-sockjs/server.js
+++ vanilla-sockjs/server.js
@@ -14,110 +14,118 @@ db.on('connect', function() {
 
 var connections = {};
 
+var handlers = {
+    init: function(conn, data) {
+        var res = {
+            connId: conn.id,
+            type: 'join',
+            nickname : data.nickname
+        };
+        conn.write(JSON.stringify(res));
+    },
+
+    join: function(conn, data) {
+        var res = {
+            connId : data.connId,
+            type : data.type,
+            nickname : data.nickname
+        }
+
+        db.get('chat_' + data.chatId, function(err, reply) {
+            var memberList = JSON.parse(reply);
+
+
+            memberList.forEach(function (c, key) {
+                var res = {
+                    type:'notiJoin',
+                    nickname : data.nickname,
+                    connId : conn.id
+                };
+
+                if(connections[c.connId]) {
+                    connections[c.connId].write(JSON.stringify(res));
+                }
+            });
+
+            var member = {
+                connId: data.connId,
+                nickname: data.nickname
+            };
+            memberList.push(member);
+
+            db.set('chat_' + data.chatId, JSON.stringify(memberList));
+        });
+    },
+
+    send: function(conn, data) {
+        db.get('chat_' + data.chatId, function(err, reply) {
+            var memberList = JSON.parse(reply);
+            memberList.forEach(function (c, key) {
+                var res = {
+                    type: 'send',
+                    nickname : data.nickname,
+                    connId : conn.id,
+                    message : data.message
+                };
+
+                if(connections[c.connId]) {
+                    connections[c.connId].write(JSON.stringify(res));
+                }
+            });
+
+        });
+
+    },
+
+    unload: function(conn, data) {
+        delete connections[conn.id];
+
+
+        db.get('chat_' + data.chatId, function(err, reply) {
+            var memberList = JSON.parse(reply);
+            var res = {
+                type: 'notiUnload'
+            };
+            var unloadMember = memberList.filter(function(member) { return member.connId == conn.id } );
+            memberList = memberList.filter(function(member) { return member.connId != conn.id } );
+            db.set('chat_' + data.chatId, JSON.stringify(memberList));
+
+
+            res.memberList = memberList;
+            res.unloadMember = unloadMember;
+
+            memberList.forEach(function (member, key) {
+                if(connections[member.connId]) {
+                    connections[member.connId].write(JSON.stringify(res));
+                }
+            });
+        });
+
+    },
+
+    timeout: function(conn, data) {
+        delete connections[conn.id];
+        db.del(['chat_' + data.chatId, 'timeout_' + data.chatId]);
+    }
+
+}
 
 echo.on('connection', function(conn) {
 
     connections[conn.id] = conn;
-    console.log(conn.id);
 
-    conn.on('data', function(data) {
-
-        var reqJson = JSON.parse(data);
-
-        if(reqJson.type == 'INIT') {
-            var res = {};
-            res.connId = conn.id;
-            res.type = 'JOIN';
-            res.nickname = reqJson.nickname;
-            conn.write(JSON.stringify(res));
-        }
-
-        if(reqJson.type == 'JOIN') {
-            var res = {};
-            res.connId = reqJson.connId;
-            res.type = reqJson.type;
-            res.nickname = reqJson.nickname;
-
-
-            db.get('chat_' + reqJson.chatId, function(err, reply) {
-                var memberList = JSON.parse(reply);
-
-
-                memberList.forEach(function (c, key) {
-                    var res = {type:'NOTI-JOIN'};
-                    res.nickname = reqJson.nickname;
-                    res.connId = conn.id;
-
-                    if(connections[c.connId]) {
-                        connections[c.connId].write(JSON.stringify(res));
-                    }
-                });
-
-                var member = {connId: reqJson.connId, nickname: reqJson.nickname};
-                memberList.push(member);
-
-
-                db.set('chat_' + reqJson.chatId, JSON.stringify(memberList));
-            });
-
-        }
-
-
-        if(reqJson.type == 'SEND') {
-            var chatId = reqJson.chatId;
-            db.get('chat_' + chatId, function(err, reply) {
-                var memberList = JSON.parse(reply);
-                memberList.forEach(function (c, key) {
-                    var res = {type: 'SEND'};
-                    res.message = reqJson.message;
-                    res.nickname = reqJson.nickname;
-                    res.connId = conn.id;
-                    if(connections[c.connId]) {
-                        connections[c.connId].write(JSON.stringify(res));
-                    }
-                });
-
-            });
-        }
-
-        if(reqJson.type == 'UNLOAD') {
-            delete connections[conn.id];
-            console.log('delete - ' + conn.id);
-
-            var chatId = reqJson.chatId;
-
-            db.get('chat_' + chatId, function(err, reply) {
-                var memberList = JSON.parse(reply);
-                var res = {type: 'NOTI-UNLOAD'};
-                var unloadMember = memberList.filter(function(member) { return member.connId == conn.id } );
-                memberList = memberList.filter(function(member) { return member.connId != conn.id } );
-                db.set('chat_' + chatId, JSON.stringify(memberList));
-
-
-                res.memberList = memberList;
-                res.unloadMember = unloadMember;
-
-
-                memberList.forEach(function (member, key) {
-                    if(connections[member.connId]) {
-                        console.log('-3- ' + member.connId);
-                        connections[member.connId].write(JSON.stringify(res));
-                    }
-                });
-            });
-        }
-
-        // timeout
-        if(reqJson.type == 'TIMEOUT') {
-            var chatId = reqJson.chatId;
-            delete connections[conn.id];
-            db.del(['chat_' + chatId, 'timeout_' + chatId]);
-        }
+    conn.on('data', function(receiveData) {
+        var data = JSON.parse(receiveData);
+        handlers[data.type](conn, data);
     });
 
     conn.on('close', function() {
     });
+
 });
+
+
+
 
 var server = http.createServer();
 echo.installHandlers(server, {prefix:'/echo'});
